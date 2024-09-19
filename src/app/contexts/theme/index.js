aba9d89b6d6e3a5e0a293b98f2b2ee128cbe8027
aeropriest <ashok.jaiswal@gmail.com>
2024-09-19 22:15:07 +0800

    * Remove the system thing

--- src/app/contexts/theme/index.js
+++ src/app/contexts/theme/index.js
@@ -5,33 +5,13 @@ import React, { createContext, useState, useContext, useEffect } from "react";
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  console.log("ThemeProvider rendered");
-
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
-    // Check for saved theme
     const savedTheme = localStorage.getItem("theme");
     if (savedTheme) {
       setIsDarkMode(savedTheme === "dark");
-    } else {
-      // Check system preference if no saved theme
-      const prefersDark = window.matchMedia(
-        "(prefers-color-scheme: dark)"
-      ).matches;
-      setIsDarkMode(prefersDark);
     }
-
-    // Listen for changes in system preference
-    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-    const handleChange = (e) => {
-      if (localStorage.getItem("theme") === null) {
-        setIsDarkMode(e.matches);
-      }
-    };
-    mediaQuery.addListener(handleChange);
-
-    return () => mediaQuery.removeListener(handleChange);
   }, []);
 
   useEffect(() => {
@@ -40,7 +20,6 @@ export const ThemeProvider = ({ children }) => {
   }, [isDarkMode]);
 
   const toggleDarkMode = () => {
-    console.log("change the theme to ", isDarkMode);
     setIsDarkMode(!isDarkMode);
   };
 
