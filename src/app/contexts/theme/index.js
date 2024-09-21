1f339237c779f2806971ae8214c191b3af0cc357
aeropriest <ashok.jaiswal@gmail.com>
2024-09-21 21:20:28 +0800

    * Fixed the saved state of theme

--- src/app/contexts/theme/index.js
+++ src/app/contexts/theme/index.js
@@ -4,38 +4,22 @@ import React, { createContext, useState, useContext, useEffect } from "react";
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [isDarkMode, setIsDarkMode] = useState(true);
+  const [isDarkMode, setIsDarkMode] = useState(null);
 
   useEffect(() => {
-    const savedTheme = localStorage.getItem("theme");
-    console.log("----- get theme-----", savedTheme);
-    if (savedTheme) {
-      setIsDarkMode(savedTheme === "dark");
-    } else {
-      // If no saved theme, check system preference
-      const prefersDark = window.matchMedia(
-        "(prefers-color-scheme: dark)"
-      ).matches;
-      setIsDarkMode(prefersDark);
-    }
-
-    // Listen for changes in system preference
-    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-    const handleChange = (e) => {
-      if (localStorage.getItem("theme") === null) {
-        setIsDarkMode(e.matches);
-      }
-    };
-    mediaQuery.addListener(handleChange);
-
-    return () => mediaQuery.removeListener(handleChange);
+    setIsDarkMode(
+      localStorage.getItem("theme")
+        ? localStorage.getItem("theme") === "dark"
+        : window.matchMedia("(prefers-color-scheme: dark)").matches
+    );
   }, []);
 
   useEffect(() => {
-    console.log("----- save theme -----", isDarkMode);
-    localStorage.setItem("theme", isDarkMode ? "dark" : "light");
-    document.body.classList.toggle("dark-mode", isDarkMode);
-    document.body.classList.toggle("light-mode", !isDarkMode);
+    if (isDarkMode !== null) {
+      localStorage.setItem("theme", isDarkMode ? "dark" : "light");
+      document.body.classList.toggle("dark-mode", isDarkMode);
+      document.body.classList.toggle("light-mode", !isDarkMode);
+    }
   }, [isDarkMode]);
 
   const toggleDarkMode = () => {
