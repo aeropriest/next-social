7fee9654120a0987a4d76d2a94a8a451a5150086
Ashok Jaiswal <ashokjaiswal@Ashoks-MacBook-Pro.local>
2024-10-07 10:34:51 +0800

    * Created the user

--- src/app/circle/page.js
+++ src/app/circle/page.js
@@ -1,24 +1,62 @@
 // Home.js
 
 "use client";
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import styles from "./page.module.scss"; // Assuming you have a separate SASS file for styles
+import { W3SSdk } from "@circle-fin/w3s-pw-web-sdk";
+let sdk;
 
 const Home = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [userData, setUserData] = useState(null);
+  useEffect(() => {
+    sdk = new W3SSdk();
+    console.log("================================");
+    console.log(process.env.NEXT_PUBLIC_CIRCLE_API_KEY);
+  }, []);
+
+  const [appId, setAppId] = useState();
+  const [userToken, setUserToken] = useState();
+  const [encryptionKey, setEncryptionKey] = useState();
+  const [challengeId, setChallengeId] = useState();
+
+  const [email, setEmail] = useState("test@newsocialtheory.com");
+  const [password, setPassword] = useState("123@Test123");
   const [isLogin, setIsLogin] = useState(false);
+
+  const onAppIdChange = useCallback((e) => {
+    setAppId(e.target.value);
+  }, []);
+  const onUserTokenChange = useCallback((e) => {
+    setUserToken(e.target.value);
+  }, []);
+  const onEncryptionKeyChange = useCallback((e) => {
+    setEncryptionKey(e.target.value);
+  }, []);
+  const onChallengeIdChange = useCallback((e) => {
+    setChallengeId(e.target.value);
+  }, []);
 
   const handleRegister = async (e) => {
     e.preventDefault();
-    try {
-      const response = await axios.post("/api/register", { email, password });
-      setUserData(response.data);
-    } catch (error) {
-      console.error(error);
-    }
+
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: "/api/user",
+      headers: {},
+    };
+
+    axios
+      .request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        setUserToken(response.data.userToken);
+        setEncryptionKey(response.data.encryptionKey);
+        setChallengeId(response.data.challengeId);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
